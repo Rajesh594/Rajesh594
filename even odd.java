68a32ef535bb42fdf186e5c5b3c0da2a8ace1d02
Rajesh594 <155128457+Rajesh594@users.noreply.github.com>
2023-12-29 16:40:21 +0530

    Add files via upload

---
+++ even odd.java
@@ -0,0 +1,19 @@
+import java .util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner S=new Scanner(System.in);
+	    int a;
+	    System.out.println("enter a no");
+	    a=S.nextInt();
+	    if(a%2==0)
+	    {
+	        System.out.println("even no");
+	    }
+	    else
+	    {
+		System.out.println("odd no");
+	}
+	}
+}
