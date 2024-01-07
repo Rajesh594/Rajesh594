158cee0339718a7834767074d2ca7142badfe02c
Rajesh594 <155128457+Rajesh594@users.noreply.github.com>
2024-01-07 16:24:56 +0530

    Add files via upload

---
+++ instance variable.java
@@ -0,0 +1,16 @@
+class sample
+{
+    int a;
+    float b;
+    boolean c;
+    String d;
+    public static void main(String[]args)
+{
+    sample s1=new sample();
+    System.out.println(s1.a);
+    System.out.println(s1.b);
+    System.out.println(s1.c);
+    System.out.println(s1.d);
+}
+    }
+
